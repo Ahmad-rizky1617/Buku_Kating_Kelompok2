41ba602c2fe017673496c7dcb61c4d4985a47f47
kengcyaw <kengracya@gmail.com>
2024-11-04 23:39:16 +0700

    benerin kreasi 1

--- tools/KREASI.py
+++ tools/KREASI.py
@@ -10,11 +10,11 @@ st.write("Di era digital saat ini, data science telah menjadi pilar utama dalam 
 ("Otomatisasi Proses: Di era digital, data science mendukung otomatisasi yang efisien, seperti dalam chatbot, asisten virtual, atau proses manufaktur otomatis. Machine learning dan AI digunakan untuk mengotomatiskan tugas rutin, yang meningkatkan produktivitas dan mengurangi biaya.")
 
 st.header("Dampak Data Science dalam Kehidupan Sehari-hari")
-st.writer("E-commerce: Perusahaan seperti Amazon menggunakan data science untuk menganalisis perilaku belanja pengguna, menyesuaikan harga, dan mengelola inventaris secara dinamis."
+st.write("E-commerce: Perusahaan seperti Amazon menggunakan data science untuk menganalisis perilaku belanja pengguna, menyesuaikan harga, dan mengelola inventaris secara dinamis."
 "Kesehatan: Data science mendukung inovasi medis, seperti prediksi penyakit dan diagnosis berbasis AI. Algoritma mempelajari pola dari jutaan data pasien untuk memberikan perawatan yang lebih tepat dan personal."
 "Media Sosial: Platform seperti Facebook dan Instagram memanfaatkan data science untuk memprediksi interaksi dan menampilkan konten yang paling relevan bagi pengguna. Ini meningkatkan keterlibatan, tetapi juga menimbulkan kekhawatiran tentang privasi dan keamanan data.")
 
 st.header("Tantangan di era digital")
-st.writer("Di sisi lain, data science di era digital juga menghadapi tantangan besar, seperti keamanan data, etika penggunaan data, dan bias algoritma. Data yang dihasilkan oleh pengguna dapat disalahgunakan jika tidak diatur dengan baik. Oleh karena itu, perlindungan privasi dan regulasi yang ketat sangat diperlukan."
+st.write("Di sisi lain, data science di era digital juga menghadapi tantangan besar, seperti keamanan data, etika penggunaan data, dan bias algoritma. Data yang dihasilkan oleh pengguna dapat disalahgunakan jika tidak diatur dengan baik. Oleh karena itu, perlindungan privasi dan regulasi yang ketat sangat diperlukan."
 
 "Peran data science terus berkembang seiring dengan munculnya teknologi baru seperti IoT, blockchain, dan edge computing. Masa depan data science kemungkinan besar akan melibatkan lebih banyak kolaborasi antar bidang untuk menciptakan solusi yang lebih holistik.")
